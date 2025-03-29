3ee8a1dfc788110a9aa5b73313984ae0829f26f5
Damon Vizl <88814126+DamonVizl@users.noreply.github.com>
2025-03-29 19:15:14 +1000

    Damon/#35LureAttraction
    + lure attraction working ish
    size is incremented
    mass is incremented
    on trigger enter exit work

--- Assets/Fishing/Lure.cs
+++ Assets/Fishing/Lure.cs
@@ -1,30 +1,42 @@
 using System.Collections.Generic;
+using Mono.Cecil.Cil;
 using UnityEngine;
+using UnityEngine.UIElements;
 
-[RequireComponent(typeof(Collider))]
+[RequireComponent(typeof(SphereCollider))]
 public class Lure : MonoBehaviour
 {
 
     [SerializeField]
     private float _magneticMass = 1; // The mass of the lure itself
+    [SerializeField] private float _radius = 20f; // The radius of the lure's magnetic field
     public float MagneticMass => _magneticMass; // The mass of the lure itself
+    SphereCollider _collider;
     
     List<IAmMagnetic> _magneticObjects = new List<IAmMagnetic>();
-
+    void Start()
+    {
+        _collider = GetComponent<SphereCollider>();
+        _collider.radius = _radius;
+    }
 
     void OnTriggerEnter(Collider other)
-    {   
-        if(other is IAmMagnetic magneticObject)
+    {       
+        Debug.Log("collision detected   " + other.name);
+        var magneticObject = other.GetComponent<IAmMagnetic>();
+        if( magneticObject is IAmMagnetic)
         {
+            Debug.Log("Magnetic object entered the trigger: " + other.name);
             AddMagneticObject(magneticObject);
         }
-        
     }
 
     void OnTriggerExit(Collider other)
     {
-        if(other is IAmMagnetic magneticObject)
+        var magneticObject = other.GetComponent<IAmMagnetic>();
+        if( magneticObject is IAmMagnetic)
         {
+            Debug.Log("Magnetic object entered the trigger: " + other.name);
             RemoveMagneticObject(magneticObject);
         }
     }
@@ -39,11 +51,18 @@ public class Lure : MonoBehaviour
     }
     /// <summary>
     /// when the magnetic object is close enough, add it's mass to the lure's mass.
+    /// increase the radius of the magnetic field
     /// </summary>
     /// <param name="magneticObject"></param>
     public void AttachMagneticToLure(IAmMagnetic magneticObject)
     {
-        if(_magneticObjects.Contains(magneticObject)) _magneticMass += magneticObject.GetMass();
+        if(_magneticObjects.Contains(magneticObject))
+        {
+            _magneticMass += magneticObject.GetMass();
+            UpdateRadius(magneticObject.GetMass());
+            Debug.Log("Magnetic object attached to lure: " + magneticObject + " mass: " + magneticObject.GetMass());
+            Debug.Log("New lure mass: " + _magneticMass);
+        } 
     }
     /// <summary>
     /// when the magnetic object is far enough
@@ -51,12 +70,28 @@ public class Lure : MonoBehaviour
     /// <param name="magneticObject"></param>
     public void RemoveMagneticFromLure(IAmMagnetic magneticObject)
     {
-        if(_magneticObjects.Contains(magneticObject)) _magneticMass -= magneticObject.GetMass();
+        if(_magneticObjects.Contains(magneticObject))
+        {
+            Debug.Log("Magnetic object removed from lure: " + magneticObject + " mass: " + magneticObject.GetMass());
+
+             _magneticMass -= magneticObject.GetMass();
+             UpdateRadius(-magneticObject.GetMass());
+        }
     }
+
+    void UpdateRadius(float mass)
+    {
+        _radius += mass* 1f; 
+        _collider.radius = _radius;
+        Debug.Log("New radius: " + _collider.radius);   
+    }
+                        
+
     void Update()
     {
         foreach(var magneticObject in _magneticObjects)
         {
+            //magneticObject.GetRigidbody().AddForce(Vector3.up * _magneticMass * 10f, ForceMode.Force);
             Vector3 direction = (transform.position - magneticObject.GetPosition()).normalized;
             float distance = Vector3.Distance(transform.position, magneticObject.GetPosition());
             magneticObject.ApplyForce(direction, distance, _magneticMass);
